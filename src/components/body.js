4126143965219e5a7ffb475aa2623bf56bf66a36
Arthur Tkachenko <atherdon@gmail.com>
2018-04-18 15:22:57 +0300

    Create body.js

---
+++ src/components/body.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Link from 'gatsby-link'
+//import logo from ''
+
+const Body = ({  }) => (
+  <div className="divtwo">
+    <div className="heading">
+      <p> Organize your shopping <br/> experience</p>
+    </div>
+    <div className="subheading">
+    <h2><small> This is Michelangelo Grocery List.Maybe we can make it better?</small></h2> </div>
+    <div className="button"><button type="button" className="btn btn-spl btn-primary btn-lg">
+      <i class="fab fa-github"></i>Get Started Now</button>
+    </div>
+    <div className="image-container">
+    <img src="" className="img img-responsive" alt = "screenshot of application"/>
+    </div>
+  </div>
+);
+export default Body
