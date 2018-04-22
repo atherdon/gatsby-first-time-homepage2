cff8b3510e9f0df252f2be2fcbe10a7c719e5973
Arthur Tkachenko <atherdon@gmail.com>
2018-04-22 20:23:29 +0300

    update_this

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo1 from '../static/Logos-01.png'
 
 const Header = ({ siteTitle }) => (
   <div
@@ -19,6 +20,13 @@ const Header = ({ siteTitle }) => (
       }}
     >
       <h1 style={{ margin: 0, textAlign: 'center', fontSize: '18px' }}>
+        <img style={{
+            width:'200px',
+            
+        }}
+             src={logo1}
+             className="img img-responsive pull-left"
+             alt="Groceristar logo" />
         <Link
           to="/"
           style={{
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -9,49 +9,58 @@ import Feature      from '../components/feature';
 // data.menu
 // data.headings
 // data.footer
-
+import logo1 from '../static/Logos-01.png'
+import logo2 from '../static/Logos-04.png'
+import groceryListImage from '../static/michelangelo-grocery-list.png'
 
 
 const HomePage = () => {
 
 	const a = data.features;
-	console.log(a)
+	// console.log(a)
 
-	// let pidors = _.each(a, (pidor) => (
- //    	<li>{pidor[0]}</li>
- //    	<li>{pidor[1]}</li>
-	// ));
-//     const numbers = [1, 2, 3, 4, 5];
+
 let listItems = _.each(data.features, (i, feature) => (
-	
+
 	<Feature key="{i}" heading="{feature[0]}" content="{feature[1]}" img="fas fa-shopping-cart fa-2x" />
-	
-	)
-);
-let listItems2 = _.each(data.headings, (i, feature) => (
-	
-	<Feature key="{i}" heading="{feature[0]}" content="{feature[1]}" img="fas fa-shopping-cart fa-2x" />
-	
-	)
-);
-let listItems3 = _.each(data.footer, (i, feature) => (
-	
-	<Feature key="{i}" heading="{feature[0]}" content="{feature[1]}" img="fas fa-shopping-cart fa-2x" />
-	
+
 	)
 );
 
+let listItems2 = _.each(data.headings, (i, heading) => (
+
+	<Feature key="{i}" heading="{heading[0]}" content="{heading[1]}" img="fas fa-shopping-cart fa-2x" />
+
+	)
+);
+
+let listItems3 = _.each(data.footer, (i, footer) => (
+
+	<Feature key="{i}" heading="{footer[0]}" content="{footer[1]}" img="fas fa-shopping-cart fa-2x" />
+
+	)
+);
 
 
 
 
 
+ // <img src={logo1} className="img img-responsive" alt="Groceristar logo" />
+ // <img src={logo2} className="img img-responsive" alt="Groceristar logo" /> 
 
  return (
- 	
+
   <div>
+
+		<img src={groceryListImage} className="img img-responsive" alt="Michelangelo grocery list" />
+
+
   	{listItems}
-  	
+		<hr />
+		{listItems2}
+		<hr />
+		{listItems3}
+		<hr />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
@@ -66,26 +75,5 @@ let listItems3 = _.each(data.footer, (i, feature) => (
 
 }
 
-
-// (
-//   <div>
-//     <h1>Hi from the second page</h1>
-//     <p>Welcome to page 2</p>
-//     <Link to="/">Go back to the homepage</Link>
-
-//     {_.each(data.features, (feature) => {
-//     	console.log(feature);
-//     	// return (
-//     	// <h1>{feature[0]}</h1>
-//     	// <h2>{feature[1]}</h2>
-//     	// );
-//     })
-// }
-
-
-
-
-//   </div>
-// )
 
 export default HomePage
