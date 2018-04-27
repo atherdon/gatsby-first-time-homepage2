8402095eee4c97f3f926c57a815a2e90923de1df
Arthur Tkachenko <atherdon@gmail.com>
2018-04-27 05:22:19 +0300

    111

--- .cache/layouts/homepage2.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "D:/code/gatsby-first-time-homepage2/src/layouts/homepage2.js"
-  import data from "D:\\code\\gatsby-first-time-homepage2\\.cache\\json\\layout-homepage-2.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "D:/code/gatsby-first-time-homepage2/src/layouts/index.js"
-  import data from "D:\\code\\gatsby-first-time-homepage2\\.cache\\json\\layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
